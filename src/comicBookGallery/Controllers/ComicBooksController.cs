d2436d1022f40e76fa6c5dcadaf59d2a24dbc7a0
Henrik Wallgren <Heeenriik@live.se>
2017-12-19 09:59:45 +0100

    added comments to properies

--- src/comicBookGallery/Controllers/ComicBooksController.cs
+++ src/comicBookGallery/Controllers/ComicBooksController.cs
@@ -13,9 +13,13 @@ namespace comicBookGallery.Controllers
         {
             var comicBook = new ComicBook()
             {
+                // Titeln
                 SeriesTitle = "The Amazing Spider Man",
+                //Vilket nummer
                 IssueNumber = 700,
+                //Beskrivning, i htmlformat
                 DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
+                // lista av "artists"
                 Artists = new Artist[]
                 {
                     new Artist() { Name = "Dan Slott", Role = "Script"},
