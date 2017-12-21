3a27f8c904b22b896f39723da0d3658586b195eb
Henrik Wallgren <Heeenriik@live.se>
2017-12-21 01:36:18 +0100

    updated comic books controller to use repository

--- src/comicBookGallery/Controllers/ComicBooksController.cs
+++ src/comicBookGallery/Controllers/ComicBooksController.cs
@@ -1,4 +1,5 @@
-﻿using comicBookGallery.Models;
+﻿using comicBookGallery.Data;
+using comicBookGallery.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,26 +10,20 @@ namespace comicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
-        public ActionResult Detail()
+        private ComicBookRepository _comicBookRepository = null;
+
+        public ComicBooksController()
         {
-            var comicBook = new ComicBook()
+            _comicBookRepository = new ComicBookRepository();
+        }
+
+        public ActionResult Detail(int? id)
+        {
+            if (id == null)
             {
-                // Titeln
-                SeriesTitle = "The Amazing Spider Man",
-                //Vilket nummer
-                IssueNumber = 700,
-                //Beskrivning, i htmlformat
-                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
-                // lista av "artists"
-                Artists = new Artist[]
-                {
-                    new Artist() { Name = "Dan Slott", Role = "Script"},
-                    new Artist() { Name = "Humberto Ramos", Role = "Pencils"},
-                    new Artist() { Name = "Victor Olazaba", Role = "Inks"},
-                    new Artist() { Name = "Edgar Delgado", Role = "Colors"},
-                    new Artist() { Name = "Chris Eliopoulos", Role = "Letters"},
-                }
-            };
+                return HttpNotFound();
+            }
+            var comicBook = _comicBookRepository.GetComicBook(id.Value);
 
             return View(comicBook);
         }
